39c84c94ded8eca94ede1e2176bbe200b9139405
ejveli <71405672+ejveli@users.noreply.github.com>
2020-09-16 20:42:32 +0100

    Add files via upload

---
+++ day_2/Question7.java
@@ -0,0 +1,15 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" /\\");
+		System.out.println("/__\\\n");
+		System.out.println("I am from \"Germany\".\n");
+		System.out.println("How is yourday going on,\'good\' or \'bad\'");
+		
+
+	}
+
+}
